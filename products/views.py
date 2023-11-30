a46afdd678cf8a774b70b2da3bae23d1343d775a
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-11-30 17:01:46 +0500

    add cart search and more

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 
-from products.models import ProductModel, CategoryModel
+from products.handler import bot
+from products.models import ProductModel, CategoryModel, CartModel
 
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
@@ -13,6 +14,14 @@ def home_page(request):
 
     context = {'products': products, 'categories': categories}
     return render(request, 'index.html', context)
+
+
+def category_page(request, pk):
+    categories = CategoryModel.objects.get(pk=pk)
+    products = ProductModel.objects.filter(product_category=categories)
+
+    context = {'products': products, 'categories': categories}
+    return render(request, 'cat_products.html', context)
 
 
 class MyLoginView(LoginView):
@@ -31,6 +40,48 @@ def logout_view(request):
 def single_product(request, pk):
     product = ProductModel.objects.get(pk=pk)
 
-
     context = {'product': product}
     return render(request, 'single-products.html', context)
+
+
+def search_products(request):
+    if request.method == 'POST':
+        get_product = request.POST.get('search_product')
+
+        try:
+            exact_product = ProductModel.objects.get(product_title__icontains=get_product)
+
+            return redirect(f'product/{exact_product.id}')
+        except:
+            return redirect('/')
+
+
+def add_products_to_user_cart(request, pk):
+    if request.method == 'POST':
+        checker = ProductModel.objects.get(pk=pk)
+
+        if checker.product_amount >= int(request.POST.get('pr_count')):
+            CartModel.objects.create(user_id=request.user.id,
+                                     user_product=checker,
+                                     user_product_quantity=int(request.POST.get('pr_count'))
+                                     ).save()
+            return redirect('/')
+        else:
+            return redirect(f'product/{checker.pk}')
+
+
+def user_cart(request):
+    cart = CartModel.objects.filter(user_id=request.user.id)
+
+    if request.method == 'POST':
+        main_text = 'Новый заказ'
+
+        for i in cart:
+            main_text += f'Товар: {i.user_product}\n' \
+                         f'Кол-во: {i.user_product_quantity}'
+            bot.send_message(575148251, main_text)
+            cart.delete()
+            return redirect('/')
+
+    else:
+        return render(request, 'cart.html', {'cart': cart})
--- shop/urls.py
+++ shop/urls.py
@@ -3,14 +3,16 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
-from products.views import home_page, MyLoginView, logout_view, single_product
+from products.views import home_page, MyLoginView, logout_view, single_product, category_page, search_products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page, name='home_page'),
     path('login', MyLoginView.as_view(), name='login'),
     path('logout', logout_view, name='logout'),
-    path('product/<int:pk>', single_product, name='single-product')
+    path('product/<int:pk>', single_product, name='single-product'),
+    path('category/<int:pk>', category_page, name='category-page'),
+    path('search', search_products, name='search-product')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
